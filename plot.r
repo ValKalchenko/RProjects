f796714e1b746676b30e7a496450d21c1f22a62c
Valerie Kalchenko <valerie.kalchenko@gmail.com>
2024-03-26 10:35:47 -0400

    3/26/2024

--- plot.r
+++ plot.r
@@ -44,3 +44,67 @@ head(iris)
 # create a plot matrix of scatter plots for all the data
 # in the iris data frame
 plot(iris)
+
+# give plot funciton qualitative variable in iris data frame
+# plot knew to generate bar chart when it was given a
+# categorical variable
+# the x-axis told us the different types of species
+# the y-axis told us the number of data points in each species
+plot(iris$Species)
+
+# give plot function quantitative variable in iris data frame
+# plot knew to create a scatter plot when it was given 1
+# quanitiative variable
+# the x-axis told us the data point ids
+# the y-axis told us the length in cm. of each data point
+plot(iris$Petal.Length)
+
+# give plot function 1 qualitative variable and 1 quanitative
+# variable in the iris data frame
+# plot knew to create a box and whisker plot when it was given
+# 1 qualitative variable and 1 quanitative variable
+# the x-axis told us the species
+# the y-axis told us the petal width range for each species
+plot(iris$Species, iris$Petal.Width)
+
+# give the plot function 2 quanitative variables in the iris
+# data frame and other options that will make the plot more
+# meaningful
+# given 2 quanitative variables plot knew to create a scatter plot
+plot(iris$Petal.Length, # data points for x-axis
+    iris$Petal.Width, # data points for y-axis
+    col = "#cc0000",
+    main = "Iris: Petal Length vs. Petal Width", # main title
+    xlab = "Petal Length", # x-axis label
+    ylab = "Petal Width", # y-axis label
+)
+
+# give plot the cosine function 
+plot(cos, # cosine function
+    0,    # starting point
+    2*pi  # ending point
+)
+
+# give plot the exponential function
+plot(exp, # exponential function
+    1,    # starting point
+    5     # ending point
+)
+
+# give plot the density normal distribution function
+plot(dnorm, # density normal distribution function
+    -3,     # starting point
+    3       # ending point
+)
+
+# give plot function density normal distribution function
+# with options
+plot(dnorm, # density normal distribution function
+    -3,     # starting point
+    3,      # ending point
+    col = "blue", # color
+    lwd = 5, # wider line width
+    main = "Standard Normal Distribution", # main title
+    xlab = "z-scores", # x-axis label
+    ylab = "Density" # y-axis label
+)
